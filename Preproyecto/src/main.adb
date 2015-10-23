21e9f4b20a2ff260d053fca80e430f1da929a13e
MiiGuee <jmiguel11457@gmail.com>
2015-10-22 19:51:21 -0500

    proyecto

--- Preproyecto/src/main.adb
+++ Preproyecto/src/main.adb
@@ -58,9 +58,9 @@ procedure Main is
 
    task body Temperatura is
    begin
-   accept continuar do
       Reset(Tm);
       for i in 1..5 loop
+         accept continuar do
          ele:=temp+1;
          temp := Random(Tm);
          if (temp < 36  or temp >38) then
@@ -68,65 +68,67 @@ procedure Main is
          else
             Put_Line("Temperatura Mala: 40");
          end if;
+            PresionArterialAsistolica.continuar4;
+         end continuar;
       end loop;
-      end continuar;
    end Temperatura;
 
    frecC:Integer:=73;
 
    task body FrecuenciaCardiaca  is
    begin
-      accept continuar1 do
       Reset(Fc);
       for i in 1..5 loop
+         accept continuar1 do
          ele1:=frecC+1;
          frecC := Random(Fc);
          if (frecC < 70  or frecC >75) then
          Put_Line("Frecuencia Cardiaca Buena: 73");
          else
-            Put_Line("Frecuencia Cardiaca  Mala: 77");
+         Put_Line("Frecuencia Cardiaca  Mala: 77");
          end if;
-         end loop;
-      end continuar1;
-      Temperatura.continuar;
+         Temperatura.continuar;
+         end continuar1;
+      end loop;
    end FrecuenciaCardiaca;
 
    frecR:Integer:=16;
 
    task body FrecuenciaRespiratoria is
    begin
-      accept continuar2 do
       Reset(Fr);
       for i in 1..5 loop
+         accept continuar2 do
          ele2:= frecR+1;
           frecR:= Random(Fr);
          if ( frecR < 12  or  frecR >20) then
          Put_Line("Frecuencia Respiratoria Buena: 15");
          else
             Put_Line("Frecuencia Respiratoria Mala: 22");
-         end if;
+            end if;
+            FrecuenciaCardiaca.continuar1;
+         end continuar2;
       end loop;
-      end continuar2;
-      FrecuenciaCardiaca.continuar1;
    end FrecuenciaRespiratoria;
+
 
    presAs:Integer:=130;
 
    task body PresionArterialSistolica is
    begin
-      accept continuar3 do
       Reset(pas);
       for i in 1..5 loop
+         accept continuar3 do
          ele3:= presAs+1;
           presAs:= Random(pas);
          if ( presAs < 120 or  presAs >140) then
          Put_Line("Presion Arterial Sistolica Buena: 130");
          else
             Put_Line("Presion Arterial Sistolica Mala: 142");
-         end if;
+            end if;
+            FrecuenciaRespiratoria.continuar2;
+         end continuar3;
          end loop;
-      end continuar3;
-      FrecuenciaRespiratoria.continuar2;
    end PresionArterialSistolica;
 
 
@@ -134,22 +136,20 @@ procedure Main is
 
    task body PresionArterialAsistolica is
       begin
-         accept continuar4 do
       Reset(pas);
       for i in 1..5 loop
+         accept continuar4 do
          ele4:= presAa+1;
           presAa:= Random(pas);
          if ( presAa < 80 or  presAa >90) then
          Put_Line("Presion Arterial Asistolica Buena: 81");
          else
             Put_Line("Presion Arterial Asistolica Mala: 91");
-         end if;
+            end if;
+            PresionArterialSistolica.continuar3;
+         end continuar4;
          end loop;
-      end continuar4;
-      PresionArterialSistolica.continuar3;
    end PresionArterialAsistolica;
-
-
 
 
 begin
