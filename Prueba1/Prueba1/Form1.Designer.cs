143c65d157d8d2241666bcf9981416879e646a06
MiiGuee <jmiguel11457@gmail.com>
2015-11-12 20:32:32 -0600

    c#

---
+++ Prueba1/Prueba1/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace Prueba1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.prueba = new System.Windows.Forms.Label();
+            this.btnOrdenar = new System.Windows.Forms.Button();
+            this.tbPrueba = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.tbBuscar = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.btnAleatorio = new System.Windows.Forms.Button();
+            this.btnQuicksort = new System.Windows.Forms.Button();
+            this.btnDesordenar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // prueba
+            // 
+            this.prueba.AutoSize = true;
+            this.prueba.Location = new System.Drawing.Point(212, 239);
+            this.prueba.Name = "prueba";
+            this.prueba.Size = new System.Drawing.Size(41, 13);
+            this.prueba.TabIndex = 0;
+            this.prueba.Text = "Prueba";
+            this.prueba.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnOrdenar
+            // 
+            this.btnOrdenar.Location = new System.Drawing.Point(111, 160);
+            this.btnOrdenar.Name = "btnOrdenar";
+            this.btnOrdenar.Size = new System.Drawing.Size(75, 23);
+            this.btnOrdenar.TabIndex = 2;
+            this.btnOrdenar.Text = "Ordenar";
+            this.btnOrdenar.UseVisualStyleBackColor = true;
+            this.btnOrdenar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // tbPrueba
+            // 
+            this.tbPrueba.Location = new System.Drawing.Point(12, 22);
+            this.tbPrueba.Name = "tbPrueba";
+            this.tbPrueba.Size = new System.Drawing.Size(28, 20);
+            this.tbPrueba.TabIndex = 3;
+            this.tbPrueba.TextChanged += new System.EventHandler(this.lbPrueba_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(58, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(28, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(108, 22);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(28, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(197, 22);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(28, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(153, 22);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(28, 20);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(241, 22);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(28, 20);
+            this.textBox5.TabIndex = 8;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(289, 22);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(28, 20);
+            this.textBox6.TabIndex = 9;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(336, 22);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(28, 20);
+            this.textBox7.TabIndex = 10;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(384, 22);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(28, 20);
+            this.textBox8.TabIndex = 11;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(430, 22);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(28, 20);
+            this.textBox9.TabIndex = 12;
+            this.textBox9.TextChanged += new System.EventHandler(this.textBox9_TextChanged);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(30, 160);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 13;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // tbBuscar
+            // 
+            this.tbBuscar.Location = new System.Drawing.Point(153, 84);
+            this.tbBuscar.Name = "tbBuscar";
+            this.tbBuscar.Size = new System.Drawing.Size(44, 20);
+            this.tbBuscar.TabIndex = 14;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(289, 87);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(64, 17);
+            this.checkBox1.TabIndex = 15;
+            this.checkBox1.Text = "Ac/Dec";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // btnAleatorio
+            // 
+            this.btnAleatorio.Location = new System.Drawing.Point(273, 160);
+            this.btnAleatorio.Name = "btnAleatorio";
+            this.btnAleatorio.Size = new System.Drawing.Size(75, 23);
+            this.btnAleatorio.TabIndex = 16;
+            this.btnAleatorio.Text = "Aleatorio";
+            this.btnAleatorio.UseVisualStyleBackColor = true;
+            this.btnAleatorio.Click += new System.EventHandler(this.btnAleatorio_Click);
+            // 
+            // btnQuicksort
+            // 
+            this.btnQuicksort.Location = new System.Drawing.Point(354, 160);
+            this.btnQuicksort.Name = "btnQuicksort";
+            this.btnQuicksort.Size = new System.Drawing.Size(75, 23);
+            this.btnQuicksort.TabIndex = 18;
+            this.btnQuicksort.Text = "Quicksort";
+            this.btnQuicksort.UseVisualStyleBackColor = true;
+            this.btnQuicksort.Click += new System.EventHandler(this.btnQuicksort_Click);
+            // 
+            // btnDesordenar
+            // 
+            this.btnDesordenar.Location = new System.Drawing.Point(192, 160);
+            this.btnDesordenar.Name = "btnDesordenar";
+            this.btnDesordenar.Size = new System.Drawing.Size(75, 23);
+            this.btnDesordenar.TabIndex = 19;
+            this.btnDesordenar.Text = "Desordenar";
+            this.btnDesordenar.UseVisualStyleBackColor = true;
+            this.btnDesordenar.Click += new System.EventHandler(this.btnDesordenar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 261);
+            this.Controls.Add(this.btnDesordenar);
+            this.Controls.Add(this.btnQuicksort);
+            this.Controls.Add(this.btnAleatorio);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.tbBuscar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbPrueba);
+            this.Controls.Add(this.btnOrdenar);
+            this.Controls.Add(this.prueba);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label prueba;
+        private System.Windows.Forms.Button btnOrdenar;
+        private System.Windows.Forms.TextBox tbPrueba;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TextBox tbBuscar;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button btnAleatorio;
+        private System.Windows.Forms.Button btnQuicksort;
+        private System.Windows.Forms.Button btnDesordenar;
+    }
+}
+
---
+++ Prueba1/Prueba1/Form1.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prueba1
+{
+    public partial class Form1 : Form
+    {
+        Random rand;
+        public Form1()
+        {
+            rand = new Random();
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            LimpiarCajas();
+            int[] arreglo = new int[10];
+            arreglo[0] = Convert.ToInt32(tbPrueba.Text);
+            arreglo[1] = Convert.ToInt32(textBox1.Text);
+            arreglo[2] = Convert.ToInt32(textBox2.Text);
+            arreglo[3] = Convert.ToInt32(textBox3.Text);
+            arreglo[4] = Convert.ToInt32(textBox4.Text);
+            arreglo[5] = Convert.ToInt32(textBox5.Text);
+            arreglo[6] = Convert.ToInt32(textBox6.Text);
+            arreglo[7] = Convert.ToInt32(textBox7.Text);
+            arreglo[8] = Convert.ToInt32(textBox8.Text);
+            arreglo[9] = Convert.ToInt32(textBox9.Text);
+
+            String cadena = ordenar(arreglo);
+            prueba.Text = cadena;
+        }
+        public string ordenar(int[] arreglo)
+        {
+            string final = "";
+            bool flip = false;
+            for (int i = 0; i < arreglo.Length; i++)//.length obtiene la longitud del arreglo
+            {
+                for (int j = i + 1; j < arreglo.Length; j++)
+                {
+                    if (checkBox1.Checked)
+                        flip = (arreglo[i] > arreglo[j]);
+
+                    else
+                        flip = (arreglo[i] < arreglo[j]);
+                    if (flip)
+                    {
+                        int aux = arreglo[i];
+                        arreglo[i] = arreglo[j];
+                        arreglo[j] = aux;
+                    }
+                }
+            }
+            for (int i = 0; i < arreglo.Length; i++)
+                final += arreglo[i] + " ";
+            acomodarCajitas(arreglo);
+            return final;
+        }
+
+        public void acomodarCajitas(int[] arreglo)
+        {
+            tbPrueba.Text = arreglo[0].ToString();
+            textBox1.Text = arreglo[1].ToString();
+            textBox2.Text = arreglo[2].ToString();
+            textBox3.Text = arreglo[3].ToString();
+            textBox4.Text = arreglo[4].ToString();
+            textBox5.Text = arreglo[5].ToString();
+            textBox6.Text = arreglo[6].ToString();
+            textBox7.Text = arreglo[7].ToString();
+            textBox8.Text = arreglo[8].ToString();
+            textBox9.Text = arreglo[9].ToString();
+        }
+        private void lbPrueba_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            LimpiarCajas();
+            int Busqueda = Convert.ToInt32(tbBuscar.Text);
+            int[] arreglo = new int[10];
+            arreglo[0] = Convert.ToInt32(tbPrueba.Text);
+            arreglo[1] = Convert.ToInt32(textBox1.Text);
+            arreglo[2] = Convert.ToInt32(textBox2.Text);
+            arreglo[3] = Convert.ToInt32(textBox3.Text);
+            arreglo[4] = Convert.ToInt32(textBox4.Text);
+            arreglo[5] = Convert.ToInt32(textBox5.Text);
+            arreglo[6] = Convert.ToInt32(textBox6.Text);
+            arreglo[7] = Convert.ToInt32(textBox7.Text);
+            arreglo[8] = Convert.ToInt32(textBox8.Text);
+            arreglo[9] = Convert.ToInt32(textBox9.Text);
+            bool encontrado = false;
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                if (arreglo[i] == Busqueda)
+                {
+                    pintarCaja(i);
+                    encontrado = true;
+                }
+
+            }
+            if (!encontrado)
+                MessageBox.Show("El elemento no existe");
+
+        }
+        public void pintarCaja(int pos)
+        {
+            switch (pos)
+            {
+                case 0: { tbPrueba.BackColor = Color.Red; } break;
+                case 1: { textBox1.BackColor = Color.Red; } break;
+                case 2: { textBox2.BackColor = Color.Red; } break;
+                case 3: { textBox3.BackColor = Color.Red; } break;
+                case 4: { textBox4.BackColor = Color.Red; } break;
+                case 5: { textBox5.BackColor = Color.Red; } break;
+                case 6: { textBox6.BackColor = Color.Red; } break;
+                case 7: { textBox7.BackColor = Color.Red; } break;
+                case 8: { textBox8.BackColor = Color.Red; } break;
+                case 9: { textBox9.BackColor = Color.Red; } break;
+                default: { MessageBox.Show("Opcion no valida"); } break;
+            }
+        }
+
+        public void LimpiarCajas()
+        {
+            tbPrueba.BackColor = Color.White;
+            textBox1.BackColor = Color.White;
+            textBox2.BackColor = Color.White;
+            textBox3.BackColor = Color.White;
+            textBox4.BackColor = Color.White;
+            textBox5.BackColor = Color.White;
+            textBox6.BackColor = Color.White;
+            textBox7.BackColor = Color.White;
+            textBox8.BackColor = Color.White;
+            textBox9.BackColor = Color.White;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void btnDesordenar_Click(object sender, EventArgs e)
+        {
+            LimpiarCajas();
+            int[] arreglo = new int[10];
+            arreglo[0] = Convert.ToInt32(tbPrueba.Text);
+            arreglo[1] = Convert.ToInt32(textBox1.Text);
+            arreglo[2] = Convert.ToInt32(textBox2.Text);
+            arreglo[3] = Convert.ToInt32(textBox3.Text);
+            arreglo[4] = Convert.ToInt32(textBox4.Text);
+            arreglo[5] = Convert.ToInt32(textBox5.Text);
+            arreglo[6] = Convert.ToInt32(textBox6.Text);
+            arreglo[7] = Convert.ToInt32(textBox7.Text);
+            arreglo[8] = Convert.ToInt32(textBox8.Text);
+            arreglo[9] = Convert.ToInt32(textBox9.Text);
+            string cadena = desordenar(arreglo);
+            prueba.Text = cadena;
+
+        }
+        public string desordenar(int[] arreglo)
+        {
+            string final = "";
+            bool flip = false;
+            for (int i = 0; i < arreglo.Length; i++)//.length obtiene la longitud del arreglo
+            {
+                for (int j = 0 + 1; j < arreglo.Length; j++)
+                {
+                    if (checkBox1.Checked)
+                        flip = (arreglo[i] > arreglo[j]);
+
+                    else
+                        flip = (arreglo[i] < arreglo[j]);
+                    if (flip)
+                    {
+                        int aux = arreglo[i];
+                        arreglo[i] = arreglo[j];
+                        arreglo[j] = aux;
+                    }
+                }
+            }
+            for (int i = 0; i < arreglo.Length; i++)
+                final += arreglo[i] + " ";
+            acomodarCajitas(arreglo);
+            return final;
+        }
+
+        private void btnAleatorio_Click(object sender, EventArgs e)
+        {
+            tbPrueba.Text = Convert.ToString(rand.Next(0, 99));
+            textBox1.Text = Convert.ToString(rand.Next(0, 99));
+            textBox2.Text = Convert.ToString(rand.Next(0, 99));
+            textBox3.Text = Convert.ToString(rand.Next(0, 99));
+            textBox4.Text = Convert.ToString(rand.Next(0, 99));
+            textBox5.Text = Convert.ToString(rand.Next(0, 99));
+            textBox6.Text = Convert.ToString(rand.Next(0, 99));
+            textBox7.Text = Convert.ToString(rand.Next(0, 99));
+            textBox8.Text = Convert.ToString(rand.Next(0, 99));
+            textBox9.Text = Convert.ToString(rand.Next(0, 99));
+
+        }
+
+        private void btnQuicksort_Click(object sender, EventArgs e)
+        {
+            LimpiarCajas();
+            int[] arreglo = new int[10];
+            arreglo[0] = Convert.ToInt32(tbPrueba.Text);
+            arreglo[1] = Convert.ToInt32(textBox1.Text);
+            arreglo[2] = Convert.ToInt32(textBox2.Text);
+            arreglo[3] = Convert.ToInt32(textBox3.Text);
+            arreglo[4] = Convert.ToInt32(textBox4.Text);
+            arreglo[5] = Convert.ToInt32(textBox5.Text);
+            arreglo[6] = Convert.ToInt32(textBox6.Text);
+            arreglo[7] = Convert.ToInt32(textBox7.Text);
+            arreglo[8] = Convert.ToInt32(textBox8.Text);
+            arreglo[9] = Convert.ToInt32(textBox9.Text);
+            string cadena = Quicksort(arreglo);
+            prueba.Text = cadena;
+
+        }
+
+        public string Quicksort(int[] arreglo)
+        {
+            string final = "";
+            int sup = (arreglo.Length) - 1;
+            int inf = 0;
+            int i = inf, j = sup, elem_div, ayuda;
+            elem_div = sup;
+            Boolean bandera = false;
+            while (!bandera)
+            {
+                while (arreglo[++i] < elem_div) ;
+                while (arreglo[--j] > elem_div) ;
+                if (i < j)
+                {
+                    ayuda = arreglo[i];
+                    arreglo[i] = arreglo[j];
+                    arreglo[j] = ayuda;
+                }
+                else
+                    bandera = true;
+            }
+
+            ayuda = arreglo[i];
+            arreglo[i] = arreglo[sup];
+            arreglo[sup] = ayuda;
+
+            for (int x = 0; x < arreglo.Length; x++)
+                final += arreglo[i] + "-";
+            acomodarCajitas(arreglo);
+            return final;
+        }
+    }
+}
